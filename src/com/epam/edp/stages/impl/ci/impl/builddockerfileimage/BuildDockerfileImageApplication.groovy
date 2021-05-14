8c698945d68c3fb11536c207368f5897479288b6
Pavel_Chekhov <pavel_chekhov@epam.com>
2021-05-14 12:40:13 +0300

    change fff

--- src/com/epam/edp/stages/impl/ci/impl/builddockerfileimage/BuildDockerfileImageApplication.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/builddockerfileimage/BuildDockerfileImageApplication.groovy
@@ -55,10 +55,10 @@ class BuildDockerfileImageApplication {
                         script.sh "cp ${context.workDir}/Dockerfile ${context.codebase.deployableModuleDir}/"
                     }
 
-                    script.sh "tar -cf target/${context.codebase.name}.tar *"
+                    script.sh "cd ${context.workDir}/target/ && tar -cf ${context.codebase.name}.tar *"
 
                     def buildResult = script.openshift.selector(buildConfigApi, "${buildconfigName}").startBuild(
-                            "--from-archive=target/${context.codebase.name}.tar",
+                            "--from-archive=${context.workDir}/target/${context.codebase.name}.tar",
                             "--wait=true")
                     resultTag = buildResult.object().status.output.to.imageDigest
                 }
