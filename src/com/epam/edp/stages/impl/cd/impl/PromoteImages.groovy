0fd01bb56eb6a9969a3bcc33511c2ba94d976abc
Pavel_Chekhov <pavel_chekhov@epam.com>
2021-06-09 18:04:19 +0300

    change PromoteImages

--- src/com/epam/edp/stages/impl/cd/impl/PromoteImages.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/PromoteImages.groovy
@@ -31,7 +31,7 @@ class PromoteImages {
                         script.println("[JENKINS][DEBUG] 2")
                         script.println("[JENKINS][DEBUG] ${codebase.inputIs}:${codebase.version}")
                         script.println("[JENKINS][DEBUG] ${codebase.outputIs}")
-                        script.openshift.tag("${codebase.inputIs}:${codebase.version}",
+                        script.openshift.tag("norn-afsp-edp/${codebase.inputIs}:${codebase.version}",
                                 "${codebase.outputIs}:${codebase.version}")
                         script.println("[JENKINS][DEBUG] 3")
                         context.workDir = new File("/tmp/${RandomStringUtils.random(10, true, true)}")
