52e3542f9b792dd2d4a5b722133e34d4f990d8df
Pavel_Chekhov <pavel_chekhov@epam.com>
2021-06-10 09:05:33 +0300

    change PromoteImages

--- src/com/epam/edp/stages/impl/cd/impl/PromoteImages.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/PromoteImages.groovy
@@ -31,7 +31,7 @@ class PromoteImages {
                         script.println("[JENKINS][DEBUG] 2")
                         script.println("[JENKINS][DEBUG] ${codebase.inputIs}:${codebase.version}")
                         script.println("[JENKINS][DEBUG] ${codebase.outputIs}")
-                        script.openshift.tag("docker-group-norn-afsp-edp.apps.dev-test.norn-afsp.projects.epam.com/norn-afsp-edp/${codebase.inputIs}:${codebase.version}",
+                        script.openshift.tag("${codebase.inputIs}:${codebase.version}",
                                 "${codebase.outputIs}:${codebase.version}")
                         script.println("[JENKINS][DEBUG] 3")
                         context.workDir = new File("/tmp/${RandomStringUtils.random(10, true, true)}")
@@ -46,7 +46,7 @@ class PromoteImages {
                         }
 
                         new CodebaseImageStreams(context, script)
-                                .UpdateOrCreateCodebaseImageStream(codebase.outputIs, "${dockerRegistryHost}/norn-afsp-edp/${codebase.outputIs}", codebase.version)
+                                .UpdateOrCreateCodebaseImageStream(codebase.outputIs, "${dockerRegistryHost}/${codebase.outputIs}", codebase.version)
 
                         script.println("[JENKINS][INFO] Image ${codebase.inputIs}:${codebase.version} has been promoted to ${codebase.outputIs}")
                     }
